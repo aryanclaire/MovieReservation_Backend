14fbe706dc523f02b863d44c5c76f6b6eefa71ec
aryanclaire <aryanteodoro@gmail.com>
2024-02-14 17:45:50 +0800

    image

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/movies', movieRoutes);
-app.use('/api/movies/seats', seatRoutes);
+app.use('/api/seats', seatRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
